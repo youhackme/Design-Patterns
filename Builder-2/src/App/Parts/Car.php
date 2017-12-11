00076c584af27fb33e624ac8d0a0e1456f99c1dc
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-12-11 21:58:44 +0400

    Builder design pattern

---
+++ Builder-2/src/App/Parts/Car.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hyder
+ * Date: 11/12/2017
+ * Time: 21:35
+ */
+
+namespace App\Parts;
+
+class Car extends Vehicle
+{
+}
---
+++ Builder-2/src/App/Parts/Wheel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hyder
+ * Date: 11/12/2017
+ * Time: 21:37
+ */
+
+namespace App\Parts;
+
+class Wheel
+{
+
+}
